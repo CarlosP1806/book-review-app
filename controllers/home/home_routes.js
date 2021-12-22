b392e2aed7979c7c44883d1e5aaca718a3b8ebc3
Carlos Paez <carlosalbertopaez2002@gmail.com>
2021-12-21 20:31:37 -0600

    Add functionality to fetch google books api from client side

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const axios = require('axios');
+require('dotenv').config();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+  res.render('homepage');
 });
+
+router.get('/search/:title', (req, res) => {
+  // Fetch google books api to get data
+  const fetchURL = `https://www.googleapis.com/books/v1/volumes?q=${req.params.title}&key=${process.env.API_KEY}`;
+  axios({
+    url: fetchURL,
+    responseType: 'json'
+  })
+    .then(response => {
+      bookData = response.data.items;
+      res.render('search_results', { title: req.params.title, bookData: bookData });
+    });
+})
 
 module.exports = router;
--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
-console.log('hello, world');
+const searchBookForm = document.querySelector('.search-book-form');
+const searchBookInput = document.querySelector('#book-name-input');
+
+searchBookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  document.location.href = `/search/${searchBookInput.value}`;
+});
