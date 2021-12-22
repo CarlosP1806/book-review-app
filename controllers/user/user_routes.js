69efc214a4606f4ffd533cc5b2a958e84101d3ac
Carlos Paez <carlosalbertopaez2002@gmail.com>
2021-12-21 21:18:40 -0600

    Create login route

--- controllers/user/user_routes.js
+++ controllers/user/user_routes.js
@@ -13,5 +13,22 @@ router.post('/', async (req, res) => {
   res.json({ token, user });
 });
 
+router.post('/login', async ({ body }, res) => {
+  const user = await User.findOne({
+    $or: [{ username: body.username }, { email: body.email }]
+  });
+  if(!user) {
+    res.status(400).json({ message: 'Cannot find user' });
+  }
+
+  // Validate password
+  if( body.password !== user.password ) {
+    res.status(400).json({ message: 'Incorrect password' });
+  }
+
+  const token = signToken(user);
+  res.json({ token, user });
+});
+
 
 module.exports = router;
