398cd843291df9eb9152337d8e06a8837dbad065
Carlos Paez <carlosalbertopaez2002@gmail.com>
2021-12-21 20:45:51 -0600

    Setup mongoose database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./controllers');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,4 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(routes);
 
-app.listen(PORT);
+db.once('open', () => {
+    app.listen(PORT);
+});
