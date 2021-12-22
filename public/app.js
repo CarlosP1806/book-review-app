7e1865c0acd87431083adfbf52f8c1b61277a8cf
Carlos Paez <carlosalbertopaez2002@gmail.com>
2021-12-21 22:36:53 -0600

    Design navbar for testing login api

--- public/app.js
+++ public/app.js
@@ -1,7 +1,55 @@
+const navbarElement = document.querySelector('.navbar');
+
 const searchBookForm = document.querySelector('.search-book-form');
 const searchBookInput = document.querySelector('#book-name-input');
+
+const loginForm = document.querySelector('.login-form');
+const usernameInput = document.querySelector('#username-input');
+const passwordInput = document.querySelector('#password-input');
 
 searchBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   document.location.href = `/search/${searchBookInput.value}`;
 });
+
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const userData = {
+    "username": usernameInput.value,
+    "password": passwordInput.value
+  };
+
+  try {
+    const response = await fetch('/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    });
+
+    if (!response.ok) {
+      throw new Error('something went wrong');
+    }
+
+    const { token, user } = await response.json();
+    console.log(user);
+
+    localStorage.setItem('id_token', token);
+    window.location.assign('/');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+function renderNavbar() {
+  const token = localStorage.getItem('id_token');
+
+  if (!!token)
+    navbarElement.innerHTML = 'logged in!';
+  else
+    navbarElement.innerHTML = 'not logged :(';
+}
+
+renderNavbar();
