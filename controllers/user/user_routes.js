a178c572c7c3ba8a50ac6ca45f9d6eee5e26c1c3
Carlos Paez <carlosalbertopaez2002@gmail.com>
2021-12-21 21:27:14 -0600

    Create route to get single user data

--- controllers/user/user_routes.js
+++ controllers/user/user_routes.js
@@ -17,18 +17,30 @@ router.post('/login', async ({ body }, res) => {
   const user = await User.findOne({
     $or: [{ username: body.username }, { email: body.email }]
   });
-  if(!user) {
+  if (!user) {
     res.status(400).json({ message: 'Cannot find user' });
+  } else {
+    // Validate password
+    if (body.password !== user.password) {
+      res.status(400).json({ message: 'Incorrect password' });
+    } else {
+      const token = signToken(user);
+      res.json({ token, user });
+    }
   }
-
-  // Validate password
-  if( body.password !== user.password ) {
-    res.status(400).json({ message: 'Incorrect password' });
-  }
-
-  const token = signToken(user);
-  res.json({ token, user });
 });
 
+// Get the current user data (if any)
+router.get('/me', authMiddleware, async ({ user=null, params}, res) => {
+  const foundUser = await User.findOne({
+    $or: [{ _id: user ? user._id : params.id }, { username: params.username}]
+  });
+
+  if(!foundUser) {
+    res.status(400).json({ message: 'Cannot find user' });
+  } else {
+    res.json(foundUser);
+  }
+});
 
 module.exports = router;
